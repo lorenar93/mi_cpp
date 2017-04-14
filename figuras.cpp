94f2d7a737cb5653046eadd1b08fc745c51a9696
lorenar93 <cindylore_1110@hotmail.com>
2017-04-13 23:08:33 -0500

    desarrollo tarea

--- figuras.cpp
+++ figuras.cpp
@@ -3,47 +3,94 @@
 
 using namespace std;
 
-// Mi objeto Figura
-
-class Figura {
-private:
-  // Tarea: Anhadir atributos altura y ancho
+class Figura{
+ protected:
+  int altura_;
+  int ancho_;
 
 public:
   // Constructor 
-  Figura( /* Tarea: Completar argumentos */ ){ 
-    // Tarea : Completar constructor
+  Figura(int altura, int ancho){
+   altura_ = altura;
+   ancho_ = ancho;
   }
 
   // Destructor, luego hablamos de eso, no le pongas cuidado por ahora
-  virtual ~Figura();
+  //virtual ~Figura();
 
   // Tarea: Crear metodos que me devuelvan los valores de altura y ancho
 
+  int mi_altura(){
+   return altura_;
+  }
+
+  int mi_ancho(){
+   return ancho_;
+  }
+
 };
 
-// Tarea: Crear Objeto Rectangulo que herede metodos, ancho y altura de Figura
-// Hint: Ancho y altura ya no seran privados, sino ...
+class Rectangulo : protected Figura{
+protected:
+public:
+ Rectangulo(int altura, int ancho) : Figura (altura, ancho){
+  altura_ = altura;
+  ancho_ = ancho;
+ }
 
-// Tarea: Crear metodo que calcule el area del objeto rectangulo.
-// Hint: El area de un rectangulo es = altura * ancho.
-// Hint: En C++ la multiplicacion se hace con el signo *
+// Metodos:
 
-// Tarea: Crear Objeto Triangulo que herede metodos, ancho y largo de Figura
-// Hint: Ancho y largo ya no seran privados, sino ...
-// Hint: El area de un triangulo es = (altura * ancho)/2.
-// Hint: En C++ la multiplicacion se hace con el signo *
-// Hint: En C++ la division se hace con el signo /
+ int mi_altura(){
+  return altura_;
+ }
+
+ int mi_ancho(){
+  return ancho_;
+ }
+
+ int area(){
+  return (altura_ * ancho_);
+ }
+
+};
+
+class Triangulo : protected Figura{
+protected:
+public:
+ Triangulo(int altura, int ancho) : Figura (altura, ancho){
+  altura_ = altura;
+  ancho_ = ancho;
+ }
+
+// Metodos:
+
+ int mi_altura(){
+  return altura_;
+ }
+
+ int mi_ancho(){
+  return ancho_;
+ }
+
+ int area(){
+  return (altura_ * ancho_) / 2;
+ }
+
+};
 
 int main(){
-  // Tarea: La funcion main debe escribir al terminal el alto de un triangulo
-  // que se llame triangulo_rectangulo con ancho 4 y alto 5.
-  // Y el ancho de un Rectangulo que se llame rectangulo_grande con ancho 4 y alto 5.
+
+ Rectangulo rectangulo_grande(4,5);
+ Triangulo triangulo_rectangulo(4,5);
+
+ cout << "El alto del triangulo rectangulo es de:" << triangulo_rectangulo.mi_altura() << endl;
+ cout << "El area del triangulo rectagulo es de:" << triangulo_rectangulo.area() << endl;
+ cout << "El area del rectangulo grande es de:" << rectangulo_grande.area() << endl;
+  
   
   // Tarea: La funcion main tambien debe escribir al terminal si el area de 
   // triangulo_rectangulo es mayor al area de rectangulo_grande. Y escribir
   // al terminal el valor de ambas areas.
-  // Hint: Escribir al terminal se hace asi: cout << algo va aqui << endl;
-  
+    
   return 0;
 }
