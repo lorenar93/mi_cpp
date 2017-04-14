0a1c0798aebc2654723f1403465a0d20e928241a
dacr26 <dacr26@gmail.com>
2017-04-13 23:42:33 -0500

    Correcciones

--- figuras.cpp
+++ figuras.cpp
@@ -4,93 +4,96 @@
 using namespace std;
 
 class Figura{
- protected:
+protected:
   int altura_;
   int ancho_;
 
 public:
   // Constructor 
   Figura(int altura, int ancho){
-   altura_ = altura;
-   ancho_ = ancho;
+    altura_ = altura;
+    ancho_ = ancho;
   }
 
-  // Destructor, luego hablamos de eso, no le pongas cuidado por ahora
-  //virtual ~Figura();
+  // Destructor, luego hablamos de eso, no le pongas cuidado por ahora (dejarlo)
+  virtual ~Figura();
 
   // Tarea: Crear metodos que me devuelvan los valores de altura y ancho
 
   int mi_altura(){
-   return altura_;
+    return altura_;
   }
 
   int mi_ancho(){
-   return ancho_;
+    return ancho_;
   }
 
 };
 
 class Rectangulo : protected Figura{
-protected:
+protected: // Si no hay atributos, no es necesario escribir protected o private.
 public:
- Rectangulo(int altura, int ancho) : Figura (altura, ancho){
-  altura_ = altura;
-  ancho_ = ancho;
- }
+  Rectangulo(int altura, int ancho) : Figura (altura, ancho){
+    altura_ = altura;
+    ancho_ = ancho;
+  }
 
-// Metodos:
+  // Metodos:
 
- int mi_altura(){
-  return altura_;
- }
+  // Estas segura que este metodo es necesario dentro de esta clase?
+  int mi_altura(){
+    return altura_;
+  }
 
- int mi_ancho(){
-  return ancho_;
- }
+  // Estas segura que este metodo es necesario dentro de esta clase?
+  int mi_ancho(){
+    return ancho_;
+  }
 
- int area(){
-  return (altura_ * ancho_);
- }
+  int area(){
+    return (altura_ * ancho_);
+  }
 
 };
 
 class Triangulo : protected Figura{
-protected:
+protected: // Si no hay atributos, no es necesario escribir protected o private.
 public:
- Triangulo(int altura, int ancho) : Figura (altura, ancho){
-  altura_ = altura;
-  ancho_ = ancho;
- }
+  Triangulo(int altura, int ancho) : Figura (altura, ancho){
+    altura_ = altura;
+    ancho_ = ancho;
+  }
 
-// Metodos:
+  // Metodos:
 
- int mi_altura(){
-  return altura_;
- }
+  // Estas segura que este metodo es necesario dentro de esta clase?
+  int mi_altura(){
+    return altura_;
+  }
 
- int mi_ancho(){
-  return ancho_;
- }
+  // Estas segura que este metodo es necesario dentro de esta clase?
+  int mi_ancho(){
+    return ancho_;
+  }
 
- int area(){
-  return (altura_ * ancho_) / 2;
- }
-
+  int area(){
+    return (altura_ * ancho_) / 2;
+  }
 };
 
 int main(){
 
- Rectangulo rectangulo_grande(4,5);
- Triangulo triangulo_rectangulo(4,5);
+  Rectangulo rectangulo_grande(4,5);
+  Triangulo triangulo_rectangulo(4,5);
 
- cout << "El alto del triangulo rectangulo es de:" << triangulo_rectangulo.mi_altura() << endl;
- cout << "El area del triangulo rectagulo es de:" << triangulo_rectangulo.area() << endl;
- cout << "El area del rectangulo grande es de:" << rectangulo_grande.area() << endl;
-  
-  
+  cout << "El alto del triangulo rectangulo es de:" << triangulo_rectangulo.mi_altura() << endl;
+  cout << "El area del triangulo rectagulo es de:" << triangulo_rectangulo.area() << endl;
+  cout << "El area del rectangulo grande es de:" << rectangulo_grande.area() << endl;
+
+
   // Tarea: La funcion main tambien debe escribir al terminal si el area de 
   // triangulo_rectangulo es mayor al area de rectangulo_grande. Y escribir
   // al terminal el valor de ambas areas.
-    
+
   return 0;
 }
